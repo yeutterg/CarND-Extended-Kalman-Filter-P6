7d49149a68d96538b1295a0b3978439c4867ae4d
Greg Yeutter <yeutterg@gmail.com>
2017-07-30 21:21:40 -0400

    update kalman filter

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -18,22 +18,55 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
+  // Predict the state
+  x_ = F_ * x_;
+  P_ = F_ * P_ * F_.transpose() + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
+  // Update the state by using Kalman Filter equations
+
+  // Kalman filter equations
+  VectorXd y = z - H_ * x_;
+  MatrixXd H_transpose = H_.transpose();
+  MatrixXd S = H_ * P_ * H_transpose + R_;
+  MatrixXd K = P_ * H_transpose * S.inverse();
+
+  // Update
+  x_ = x_ + K * y;
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Extended Kalman Filter equations
-  */
+  // Update the state by using Extended Kalman Filter equations
+
+  // Calculate px^2 and py^2
+  x0_2 = x_(0) * x_(0);
+  x1_2 = x_(1) * x_(1);
+
+  // Get rho, phi, and rho dot
+  float rho = sqrt(x0_2 * x1_2);
+  float phi = atan2(x_(1), x_(0));
+  float rhodot = 0;
+  if (rho >= 0.0001 || rho <= -0.0001) {
+    pxvx = x_(0) * x_(2);
+    pyvy = x_(1) * x_(3);
+    rhodot = (pxvx + pyvy) / rho;
+  }
+
+  // Extended Kalman Filter Equations
+  VectorXd zp(3);
+  zp << rho, phi, rhodot;
+  VectorXd y = z - zp;
+  MatrixXd H_transpose = H_.transpose();
+  MatrixXd S = H_ * P_ * H_transpose + R_;
+  MatrixXd K = P_ * H_transpose * S.inverse();
+
+  // Update
+  x_ = x_ + K * y;
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
